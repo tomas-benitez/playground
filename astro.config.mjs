6e291ef793faa14670b38f36b06265af21fb3b52
DesarrolloDMIX <dev@digitalmix.ar>
2023-11-09 13:57:34 -0300

    site and base_url based on environment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+import { loadEnv } from "vite";
+const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: "https://tomas-benitez.github.io",
-  base: "/playground",
+  site: env.PUBLIC_SITE || undefined,
+  base: env.PUBLIC_BASE_URL || undefined,
 });
